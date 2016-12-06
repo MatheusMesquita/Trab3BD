03fa6c685cd65bf46292242fe159fd6e01d9e2c2
Matheus Mesquita <matheus.mesquita.santos@usp.br>
2016-12-06 19:40:01 -0200

    initial commit

---
+++ src/models/Atleta.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+/**
+ *
+ * @author EMAX
+ */
+public class Atleta {
+    private int altura;
+    private int peso;
+
+    public int getAltura() {
+        return altura;
+    }
+
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+
+    public int getPeso() {
+        return peso;
+    }
+
+    public void setPeso(int peso) {
+        this.peso = peso;
+    }
+    
+    
+}
