3fc32fec16c6241a0359c2c7828b4b62c0c9d821
MatheusMesquita <matheus.mesquita.santos@usp.br>
2016-12-07 12:34:16 -0200

    Mudança no Dumb para Data

---
+++ src/data/Data.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package data;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+import models.Esporte;
+import models.Modalidade;
+
+/**
+ *
+ * @author EMAX
+ */
+public class Data {
+    
+    public Data() {
+        
+    }
+    
+    public List<Modalidade> getAllModalidades() {
+        return null;
+    }
+    
+    public List<Esporte> getAllEsportes() {
+        return null;
+    }
+}
